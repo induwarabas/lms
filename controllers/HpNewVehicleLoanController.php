7540114627f44de4b1acc785fb881e08021e4c45
Supun Induwara <induwarabas@gmail.com>
2017-09-03 11:11:38 +0530

    Updated

--- controllers/HpNewVehicleLoanController.php
+++ controllers/HpNewVehicleLoanController.php
@@ -363,8 +363,36 @@ class HpNewVehicleLoanController extends LmsController
         $template = Template::findOne($templateId);
         $m = new \Mustache_Engine();
         $m->addHelper("format", new MustacheFormatter());
-        $text = $m->render($template->content, ['loan' => $loan, 'loan']);
+        $text = $m->render($template->content, ['loan' => $loan, 'loanx' => $loanx, 'customer' => $customer]);
+        $pdf = new Pdf([
+            // set to use core fonts only
+            'mode' => Pdf::MODE_UTF8,
+            // A4 paper format
+            'format' => Pdf::FORMAT_A4,
+            // portrait orientation
+            'orientation' => Pdf::ORIENT_PORTRAIT,
+            // stream to browser inline
+            'destination' => Pdf::DEST_BROWSER,
+            // your html content input
+            'content' => $text,
+            // format content from your own css file if needed or use the
+            // enhanced bootstrap css built by Krajee for mPDF formatting
+            'cssFile' => 'css/report.css',
+            // any css to be embedded if required
+            'cssInline' => '.kv-heading-1{font-size:18px}',
+            // set mPDF properties on the fly
+            'options' => ['title' => 'Cash Receipt'],
+            // call mPDF methods on the fly
+            'methods' => [
+//                'SetHeader'=>['Krajee Report Header'],
+//                'SetFooter'=>['{PAGENO}'],
+            ],
+            'defaultFont' => 'kaputaunicode.ttf',
+            'options' => ['currentLang' =>  'sin']
+        ]);
 
+        // return the pdf output as per the destination setting
+        return $pdf->render();
     }
 
     /**
